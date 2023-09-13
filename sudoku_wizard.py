6c0e4c6bd37053d97861447c6318e3d6084ecc53
Sreeram P <psreeram3015@proton.me>
2023-09-13 12:01:23 +0530

    Added function to check the validity of a number in a Sudoku grid position

--- sudoku_wizard.py
+++ sudoku_wizard.py
@@ -32,3 +32,26 @@ def locate_empty_cell(s_grid):
     for r in range(9):      #row
         for c in range(9):  #column
             return(r, c)
+
+#function to check the validity
+def check_validity(s_grid, num, pos):
+    #Check row
+    for r in range(9):
+        if s_grid[pos[0]][r] == num and pos[1] != r:
+            return False
+
+    #Check column
+    for c in range(9):
+        if s_grid[c][pos[1]] == num and pos[0] != c:
+            return False
+
+    #Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if s_grid[i][j] == num and (i, j) != pos:
+                return False
+    
+    return True
